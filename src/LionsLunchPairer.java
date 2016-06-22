529c6d5856f054c8fd490519b4c9ec9298da3da2
kingjkang <justin.d.kang@gmail.com>
2016-06-22 15:26:18 -0500

    first commit

---
+++ src/LionsLunchPairer.java
@@ -0,0 +1,12 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Created by justinkang on 6/22/16.
+ */
+public class LionsLunchPairer {
+    public static void main(String [] args){
+        System.out.println("testing adding justin to the db");
+        ArrayList<LionsLunchMember> members;
+    }
+}
